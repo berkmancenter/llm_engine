b57ba285e9071da098d38f708b4c1a34e18acdb4
Johnny Richardson <johnny.c.rich@gmail.com>
2026-04-24 17:43:23 -0400

    fix(roles): merge gone wrong

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -43,57 +43,9 @@ const baseRights = [
   'getExperimentResults'
 ]
 const allRoles = {
-<<<<<<< jr/feat/dynamic-responses
-  user: [
-    'createMessage',
-    'userTopics',
-    'createTopic',
-    'deleteTopic',
-    'updateTopic',
-    'createConversation',
-    'userConversations',
-    'activeConversations',
-    'ping',
-    'followConversation',
-    'followTopic',
-    'allTopics',
-    'publicConversations',
-    'topicConversations',
-    'deleteConversation',
-    'getConversation',
-    'getConversationReport',
-    'deleteTranscript',
-    'pauseTranscript',
-    'resumeTranscript',
-    'getTranscript',
-    'vote',
-    'managePseudonym',
-    'manageAccount',
-    'getUser',
-    'updateConversation',
-    'exportOwnConversation',
-    'patchConversationAgent',
-    'startConversation',
-    'stopConversation',
-    'joinConversation',
-    // poll roles
-    'createPoll',
-    'respondPoll',
-    'listPolls',
-    'inspectPoll',
-    'getPollResponses',
-    'getPollResponseCounts',
-    'createExperiment',
-    'runExperiment',
-    'getExperiment',
-    'getExperimentResults'
-  ],
-  admin: ['getConversation', 'getUsers', 'manageUsers', 'publicConversations']
-=======
   user: baseRights,
   // Admins inherit all user rights
   admin: [...baseRights, 'getUsers', 'manageUsers']
->>>>>>> main
 }
 const roles = Object.keys(allRoles)
 const roleRights = new Map(Object.entries(allRoles))
